cea7fd2bada4284b16331779eb3ffad6d893935e
markrojas <rojas.mm@gmail.com>
2019-02-08 11:25:16 -0600

    Update Springboard Projects

---
+++ Production_Data_Science/setup.py
@@ -0,0 +1,38 @@
+#from distutils.core import setup
+from setuptools import setup
+
+def readme():
+    """Import the README.md Markdown file and try to convert it to RST format."""
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError):
+        with open('README.md') as readme_file:
+            return readme_file.read()
+
+
+setup(
+    name='titanic',
+    version='0.1',
+    description='Analysis of the Titanic dataset',
+    long_description=readme(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    url='https://github.com/markrojas/titanic_datascience',
+    author='Mark Rojas',
+    author_email='rojas.mm@gmail.com',
+    license='MIT',
+    packages=['titanic'],
+    install_requires=[
+	'pypandoc>=1.4',
+	'pytest>=3.2.3',
+	'pytest-runner>=2.12.1',
+	'click>=7.0'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    entry_points='''
+        [console_scripts]
+        titanic_analysis=titanic.command_line:titanic_analysis
+    ''',
+)
